4e0ce752c3cb6199dc786fcd3011bfa37c4f52ce
Mohammed <mohammedaalli088@gmail.com>
2024-06-28 11:00:19 +0000

    ref: settings updated

--- Todo/settings.py
+++ Todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--p6+5pmax55(d28$k436zo_w7^gc8^o61+y4kt5p8%y_=$j_gq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","todo4mpedigree.pythonanywhere.com"]
 
 # Application definition
 
